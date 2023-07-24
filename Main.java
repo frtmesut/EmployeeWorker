e447aa1daece1f99c5faa05702dc862ba297be73
frtmesut <105629033+frtmesut@users.noreply.github.com>
2023-07-25 00:01:31 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package Employee;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker worker = new Worker("Mesut", 9500, 40, 2021);
+        System.out.println(worker);
+    }
+}
